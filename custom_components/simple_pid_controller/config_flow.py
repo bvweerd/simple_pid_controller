c9bde7a8f9e2e8474fc595285fb2151882607b8c
bvweerd <b.van.weerd@gmail.com>
2025-05-09 18:31:04 +0200

    update config flow

--- custom_components/simple_pid_controller/config_flow.py
+++ custom_components/simple_pid_controller/config_flow.py
@@ -1,17 +1,15 @@
 from homeassistant import config_entries
 from homeassistant.const import CONF_NAME
-from homeassistant.core import callback
-from homeassistant.helpers.selector import selector
+from homeassistant.helpers import config_validation as cv
 import voluptuous as vol
 
 from .const import DOMAIN, CONF_SENSOR_ENTITY_ID
 
-
 STEP_USER_DATA_SCHEMA = vol.Schema({
-    vol.Required(CONF_NAME): str,
-    vol.Required(CONF_SENSOR_ENTITY_ID): selector({"entity": {"domain": "sensor"}}),
+    vol.Required(CONF_NAME): cv.string,
+    # fallback to simple entity_id validation
+    vol.Required(CONF_SENSOR_ENTITY_ID): cv.entity_id,
 })
-
 
 @config_entries.HANDLERS.register(DOMAIN)
 class SimplePIDConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -20,7 +18,7 @@ class SimplePIDConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
+        """Initial step: ask for name and sensor."""
         if user_input is not None:
             return self.async_create_entry(
                 title=user_input[CONF_NAME],
@@ -29,10 +27,14 @@ class SimplePIDConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     CONF_SENSOR_ENTITY_ID: user_input[CONF_SENSOR_ENTITY_ID],
                 },
             )
-        return self.async_show_form(step_id="user", data_schema=STEP_USER_DATA_SCHEMA)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=STEP_USER_DATA_SCHEMA
+        )
 
     @staticmethod
-    @callback
+    @config_entries.callback
     def async_get_options_flow(config_entry):
         from .options_flow import SimplePIDOptionsFlowHandler
         return SimplePIDOptionsFlowHandler(config_entry)
